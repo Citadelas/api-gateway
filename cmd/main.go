25081e30d1b0ccda83a295449ce3238ea1b49f64
muerewa <ffsocietyy@protonmail.com>
2025-08-30 23:15:54 +0300

    added logger setup

--- cmd/main.go
+++ cmd/main.go
@@ -7,25 +7,26 @@ import (
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"log"
+	"log/slog"
+	"os"
 	"time"
 )
 
-func mustGenerateClient(endpoint string, timeout time.Duration) *grpc.ClientConn {
-	conn, err := grpc.NewClient(endpoint,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithConnectParams(grpc.ConnectParams{
-			MinConnectTimeout: timeout,
-		}),
-	)
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	return conn
-}
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log.Info("Starting app",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+	)
+
 	ssoConn := mustGenerateClient(cfg.Services.SSO.Endpoint, cfg.Services.SSO.Timeout)
 	defer ssoConn.Close()
 	ssoClient := ssov1.NewAuthClient(ssoConn)
@@ -44,6 +45,38 @@ func main() {
 
 	err := r.Run(cfg.Addr)
 	if err != nil {
-		log.Fatalf(err.Error())
+		panic(err)
 	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
+
+func mustGenerateClient(endpoint string, timeout time.Duration) *grpc.ClientConn {
+	conn, err := grpc.NewClient(endpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithConnectParams(grpc.ConnectParams{
+			MinConnectTimeout: timeout,
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return conn
 }
